2fb591ef9c577b5c323ccb8d5aa75403b6d8b665
wangyiming <953082389@qq.com>
2017-10-11 20:41:28 +0800

    添加热键检索F建

--- js/index.js
+++ js/index.js
@@ -38,15 +38,25 @@ $(function () {
 document.onkeydown = function (event) {
     var e = event || window.event || arguments.callee.caller.arguments[0];
     if (e && e.keyCode == 27) { // 按 Esc
-        //要做的事情     
+        //要做的事情
         alert("esc");
     }
     if (e && e.keyCode == 113) { // 按 F2
         //要做的事情
         alert("f2");
     }
-    if (e && e.keyCode == 13) { // enter 键
+    if (e && e.keyCode == 70) { // enter 键
         //要做的事情
-        alert("enter");
+        // alert("enter");
+        var searchWord = prompt("请输入检索单词", ""); //将输入的内容赋给变量 name ，
+        if(searchWord != null && searchWord != ''){
+            window.open("https://www.baidu.com/s?wd="+searchWord);
+        }
+
+        //这里需要注意的是，prompt有两个参数，前面是提示的话，后面是当对话框出来后，在对话框里的默认值
+        // if (name)//如果返回的有内容
+        // {
+        //     alert("欢迎您：" + name)
+        // }
     }
 };
